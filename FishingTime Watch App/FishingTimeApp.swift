3cc886d2cf93aca078d1ba5b1cd00e27736aadd9
ranggasaputra <ranggasaputra200412@gmail.com>
2024-05-14 15:12:57 +0700

    Initial Commit

---
+++ FishingTime Watch App/FishingTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishingTimeApp.swift
+//  FishingTime Watch App
+//
+//  Created by Rangga Saputra on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct FishingTime_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
