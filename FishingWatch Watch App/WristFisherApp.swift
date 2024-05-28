d95b54823de6bc7a599b8bef023f141096d64d93
ranggasaputra <ranggasaputra200412@gmail.com>
2024-05-28 09:49:48 +0700

    Changing name to WristFisher

--- FishingWatch Watch App/WristFisherApp.swift
+++ FishingWatch Watch App/WristFisherApp.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct FishingWatch_Watch_AppApp: App {
+struct WristFisher_Watch_AppApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
